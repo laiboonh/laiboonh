058bad5aa5091d3f7e31fa425ab73040d8a20488
Boon <laiboonh@gmail.com>
2023-09-16 21:18:06 +0800

    Fix github action and cache dialyxir PLT

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Laiboonh.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
